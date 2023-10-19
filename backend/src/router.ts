279e9b5ffaef9e0fd70dbed6f0d174e08e5f1dc6
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-10-19 12:39:42 +0200

    added ts to backend

---
+++ backend/src/router.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Tool routes
+router.get("/tool", () => {})
+router.get("/tool/:id", () => {})
+router.put("/tool/:id", () => {})
+router.get("/post", () => {})
+router.delete("/tool/:id", () => {})
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./router.js";
 
 const app = express();
 
@@ -6,5 +7,7 @@ app.get("/", (req, res) => {
   res.status(200);
   res.json({ message: "Hello World!" });
 });
+
+app.use("/api", router);
 
 export default app;
