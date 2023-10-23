bf0e5cadd61980b2370ddc708f822d6eca46b0a3
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-10-23 09:41:12 +0200

    fixed height for card component

--- src/components/app-card.ts
+++ src/components/app-card.ts
@@ -22,6 +22,7 @@ export class AppCardElement extends LitElement {
     css`
       .card {
         display: flex;
+        height: 100%;
         flex-direction: column;
         justify-content: space-between;
         align-items: center;
